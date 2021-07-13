5ee5aff134be9a2db426d9dd2c10373e88632b1a
Nicolas Noriega <nicolasn8171@gmail.com>
2021-07-12 21:44:48 -0700

    Initial commit

---
+++ src/emailApp.java
@@ -0,0 +1,11 @@
+public class emailApp
+{
+
+    public static void main(String[] args)
+    {
+
+
+
+        emailMain newEmail = new emailMain("Nick", "Noriega");
+    }
+}
---
+++ src/emailMain.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class emailMain
+{
+    private String firstName, lastName;
+    private String password;
+    private String departmentCode;
+    private int mailBoxCapacity;
+    private String altEmail;
+
+    public emailMain(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        System.out.println("Email generated: " + firstName + "." + lastName + "@xeron.com");
+
+        this.departmentCode = getDepartmentCode();
+    }
+
+    private String getDepartmentCode()
+    {
+        System.out.println("Select your department from the codes displayed below \n 1 for Sales \n 2 for Engineering \n 3 for Marketing \n 4 for Accounting \n 0 to quit");
+
+        Scanner input = new Scanner(System.in);
+        int userChoice = input.nextInt();
+
+        if(userChoice == 1) { return "Sales"; }
+        else if(userChoice == 2) { return "Engineering"; }
+        else if(userChoice == 3) { return "Marketing"; }
+        else if(userChoice == 4) { return "Accounting"; }
+        else if(userChoice == 0) { return "Quit"; }
+        else { return ""; }
+
+    }
+
+    private String randomPassword(int length)
+    {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_*";
+        char[] password = new char[length];
+
+        for(int i = 0; i < length; i++)
+        {
+            int rand = (int) (Math.random() + passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+
+        return new String(password);
+    }
+
+
+
+
+
+}
