f4fe4a8117fd02be1df38dc6a51e28aee9603390
Nicolas Noriega <nicolasn8171@gmail.com>
2021-07-17 02:58:24 -0700

    Finished code in main class

--- src/emailApp.java
+++ src/emailApp.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class emailApp
 {
 
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+
+
+
 
         emailMain newEmail = new emailMain("Nick", "Noriega");
     }
